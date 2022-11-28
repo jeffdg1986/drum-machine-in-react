8e0caeda788df8f81b73b89294e5dd171ae399a0
JPaul <jeffdg1986@gmail.com>
2022-11-27 19:05:31 -0600

    edit css and added on/off switch

--- src/PercussionBox.js
+++ src/PercussionBox.js
@@ -1,5 +1,11 @@
-import './App.css';
+// add an on/of switch that plays a old crappy machine sound when turning on. have the styling of the change to a glov when on
+// don't allow the buttons to work when the machine is off
+// add the rest of the keyboard. essentially make several instruments with the choice being a dropdown or something like that
+
+
+import './PercussionBox.css';
 import React, { Component } from 'react';
+import peptidePic from './pictures/peptide.png';
 import percussion1 from "./audio-files/percussion1.mp3";
 import percussion2 from "./audio-files/percussion2.mp3";
 import percussion3 from "./audio-files/percussion3.mp3";
@@ -13,7 +19,9 @@ class PercussionBox extends Component {
   constructor(props){
     super(props);
     this.state = { 
-      filePlaying: 'Select a sound',
+      filePlaying: '',
+      power: 'OFF',
+      backgroundColor: 'red'
    };
   }
 
@@ -25,6 +33,7 @@ componentDidMount() {
  }
 
 handleClick = (e) =>{
+  if(this.state.power === 'ON'){
   console.log(e.target.innerHTML)
   // connects the text of the clicked button to its audio file, then plays it
   document.getElementById(e.target.innerHTML.slice(0,1)).play() 
@@ -32,49 +41,68 @@ handleClick = (e) =>{
 this.setState({
   filePlaying: e.target.value
 })
+  }
 };
 
 handleKeyPress = (e) => {
-
+if(this.state.power === 'ON'){
   document.getElementById(e.key.toUpperCase()).play();
   this.setState({
     filePlaying: document.getElementById(e.key.toUpperCase()).parentNode.value
   })
-
+}
+}
+power = () =>{
+  if(this.state.power === 'OFF'){
+    this.setState({
+      power: 'ON',
+      filePlaying: 'Select a sound',
+      backgroundColor: 'green'
+    })
+  }
+  else if(this.state.power === 'ON'){
+    this.setState({
+      power: 'OFF',
+      filePlaying: '',
+      backgroundColor: 'red'
+    })
+  }
 }
   render() { 
     return (<>
-      <h1>Percussion Sounds</h1>
-      <img src="https://img.icons8.com/sf-regular/344/peptide.png" alt='https://www.google.com/' />
-<main>
-<div id="container">
-
-  <section id="drum-machine">
-  <div>
-    <button onClick={this.handleClick} className ="drum-pad" id="q-audio" value = "percussion 1">Q<audio src= {percussion1} className ="clip" id="Q"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad" id="w-audio" value = "percussion 2">W<audio src= {percussion2} className ="clip" id="W"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad" id="e-audio" value = "percussion 3">E<audio src= {percussion3} className ="clip" id="E"></audio></button>
+<main className='environment'>
+<h2>Percussion Sounds</h2>
+<img src={peptidePic} alt='https://img.icons8.com/sf-regular/344/peptide.png' className='peptide'/>
+<div className="container">
+  <div className='power'>Power Switch:
+    <button className='powerSwitch' onClick={this.power}>{this.state.power}</button>
+    <div className='powerLight' style={{backgroundColor: this.state.backgroundColor}}></div>
   </div>
-  <div>
-    <button onClick={this.handleClick} className ="drum-pad" id="a-audio" value = "percussion 4">A<audio src= {percussion4} className ="clip" id="A"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad" id="s-audio" value = "percussion 5">S<audio src= {percussion5} className ="clip" id="S"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad" id="d-audio" value = "percussion 6">D<audio src= {percussion6} className ="clip" id="D"></audio></button>
-  </div>
-  <div>
-    <button onClick={this.handleClick} className ="drum-pad" id="z-audio" value = "percussion 7">Z<audio src= {percussion7} className ="clip" id="Z"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad" id="x-audio" value = "percussion 8">X<audio src= {percussion8} className ="clip" id="X"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad" id="c-audio" value = "percussion 9">C<audio src= {percussion9} className ="clip" id="C"></audio></button>
-  </div>
-  </section>
-  <div id="display-container">
+<div className="display">
     <section id="toggle-area">
-      <h3 id='display' value="testing">{this.state.filePlaying}</h3>
+      <div id='display' value="testing">{this.state.filePlaying}</div>
     </section>
   </div>
+  <section id="drum-machine">
+  <div className='firstRow'>
+    <button onClick={this.handleClick} className ="drum-pad col1" id="q-audio" value = "Percussion 1">Q<audio src= {percussion1} className ="clip" id="Q"></audio></button>
+    <button onClick={this.handleClick} className ="drum-pad col2" id="w-audio" value = "Percussion 2">W<audio src= {percussion2} className ="clip" id="W"></audio></button>
+    <button onClick={this.handleClick} className ="drum-pad col3" id="e-audio" value = "Percussion 3">E<audio src= {percussion3} className ="clip" id="E"></audio></button>
+  </div>
+  <div className='secondRow'>
+    <button onClick={this.handleClick} className ="drum-pad col1" id="a-audio" value = "Percussion 4">A<audio src= {percussion4} className ="clip" id="A"></audio></button>
+    <button onClick={this.handleClick} className ="drum-pad col2" id="s-audio" value = "Percussion 5">S<audio src= {percussion5} className ="clip" id="S"></audio></button>
+    <button onClick={this.handleClick} className ="drum-pad col3" id="d-audio" value = "Percussion 6">D<audio src= {percussion6} className ="clip" id="D"></audio></button>
+  </div>
+  <div className='thirdRow'>
+    <button onClick={this.handleClick} className ="drum-pad col1" id="z-audio" value = "Percussion 7">Z<audio src= {percussion7} className ="clip" id="Z"></audio></button>
+    <button onClick={this.handleClick} className ="drum-pad col2" id="x-audio" value = "Percussion 8">X<audio src= {percussion8} className ="clip" id="X"></audio></button>
+    <button onClick={this.handleClick} className ="drum-pad col3" id="c-audio" value = "Percussion 9">C<audio src= {percussion9} className ="clip" id="C"></audio></button>
+  </div>
+  </section>
   </div>
   <footer>Audio files from <a href="https://www.zapsplat.com">Zapsplat</a> and <a href="https://freesound.org">freesound</a></footer>
   </main>
-
     </>);
   }
   
