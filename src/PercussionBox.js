b7b8f0b9691d83812040e117f378a74fc1d21ff1
JPaul <jeffdg1986@gmail.com>
2022-12-12 01:35:05 -0600

    created responsive grid display

--- src/PercussionBox.js
+++ src/PercussionBox.js
@@ -1,9 +1,8 @@
 // add an on/of switch that plays a old crappy machine sound when turning on. have the styling of the change to a glov when on
-// don't allow the buttons to work when the machine is off
 // add the rest of the keyboard. essentially make several instruments with the choice being a dropdown or something like that
 
 
-import './PercussionBox.css';
+import './drum-machine-dos.css';
 import React, { Component } from 'react';
 import peptidePic from './pictures/peptide.png';
 import percussion1 from "./audio-files/percussion1.mp3";
@@ -70,40 +69,28 @@ power = () =>{
 }
   render() { 
     return (<>
-<main className='environment'>
-<h2>Percussion Sounds</h2>
-<img src={peptidePic} alt='https://img.icons8.com/sf-regular/344/peptide.png' className='peptide'/>
 <div className="container">
-  <div className='power'>Power Switch:
-    <button className='powerSwitch' onClick={this.power}>{this.state.power}</button>
-    <div className='powerLight' style={{backgroundColor: this.state.backgroundColor}}></div>
+  <div className='branding'>
+  <div className='myName'>Jeff's Percussion Sounds</div>
+  <div className='grid-container'>
+      <img src={peptidePic} alt='https://img.icons8.com/sf-regular/344/peptide.png' className='peptide'/>
+      <div className='power'>Power Switch:</div>
+      <button className='powerSwitch' onClick={this.power} style={{backgroundColor: this.state.backgroundColor}}>{this.state.power}</button>
+      <div className='display' value='testing'>{this.state.filePlaying}</div>
+      <button onClick={this.handleClick} className ="Q" id="q-audio" value = "Percussion 1">Q<audio src= {percussion1} className ="clip" id="Q"></audio></button>
+      <button onClick={this.handleClick} className ="W" id="w-audio" value = "Percussion 2">W<audio src= {percussion2} className ="clip" id="W"></audio></button>
+      <button onClick={this.handleClick} className ="E" id="e-audio" value = "Percussion 3">E<audio src= {percussion3} className ="clip" id="E"></audio></button>
+      <button onClick={this.handleClick} className ="A" id="a-audio" value = "Percussion 4">A<audio src= {percussion4} className ="clip" id="A"></audio></button>
+      <button onClick={this.handleClick} className ="S" id="s-audio" value = "Percussion 5">S<audio src= {percussion5} className ="clip" id="S"></audio></button>
+      <button onClick={this.handleClick} className ="D" id="d-audio" value = "Percussion 6">D<audio src= {percussion6} className ="clip" id="D"></audio></button>
+      <button onClick={this.handleClick} className ="Z" id="z-audio" value = "Percussion 7">Z<audio src= {percussion7} className ="clip" id="Z"></audio></button>
+      <button onClick={this.handleClick} className ="X" id="x-audio" value = "Percussion 8">X<audio src= {percussion8} className ="clip" id="X"></audio></button>
+      <button onClick={this.handleClick} className ="C" id="c-audio" value = "Percussion 9">C<audio src= {percussion9} className ="clip" id="C"></audio></button>
   </div>
-<div className="display">
-    <section id="toggle-area">
-      <div id='display' value="testing">{this.state.filePlaying}</div>
-    </section>
-  </div>
-  <section id="drum-machine">
-  <div className='firstRow'>
-    <button onClick={this.handleClick} className ="drum-pad col1" id="q-audio" value = "Percussion 1">Q<audio src= {percussion1} className ="clip" id="Q"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad col2" id="w-audio" value = "Percussion 2">W<audio src= {percussion2} className ="clip" id="W"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad col3" id="e-audio" value = "Percussion 3">E<audio src= {percussion3} className ="clip" id="E"></audio></button>
-  </div>
-  <div className='secondRow'>
-    <button onClick={this.handleClick} className ="drum-pad col1" id="a-audio" value = "Percussion 4">A<audio src= {percussion4} className ="clip" id="A"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad col2" id="s-audio" value = "Percussion 5">S<audio src= {percussion5} className ="clip" id="S"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad col3" id="d-audio" value = "Percussion 6">D<audio src= {percussion6} className ="clip" id="D"></audio></button>
-  </div>
-  <div className='thirdRow'>
-    <button onClick={this.handleClick} className ="drum-pad col1" id="z-audio" value = "Percussion 7">Z<audio src= {percussion7} className ="clip" id="Z"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad col2" id="x-audio" value = "Percussion 8">X<audio src= {percussion8} className ="clip" id="X"></audio></button>
-    <button onClick={this.handleClick} className ="drum-pad col3" id="c-audio" value = "Percussion 9">C<audio src= {percussion9} className ="clip" id="C"></audio></button>
-  </div>
-  </section>
-  </div>
-  <footer>Audio files from <a href="https://www.zapsplat.com">Zapsplat</a> and <a href="https://freesound.org">freesound</a></footer>
-  </main>
-    </>);
+</div>
+<footer>Audio files from <a href="https://www.zapsplat.com">Zapsplat</a> and <a href="https://freesound.org">freesound</a></footer>
+</div>
+</>);
   }
   
 }
